e8c16baf54a2b6f1335fa6b0be3ed29b0954e084
Doron Shiffer-Sebba <shdoron1@gmail.com>
2015-04-10 18:02:48 -0400

    New fetcher for LegiScan

--- fetcher.py
+++ fetcher.py
@@ -1,7 +1,6 @@
 import csv
-from urllib2 import Request, urlopen, URLError
-
-
+import requests # requests needs to be installed for this to work ($ git clone git://github.com/kennethreitz/requests.git)
+import json
 
 def getStates():
     # Returns list of LegiScan Abbreviated State names from LegiScan source csv
@@ -10,16 +9,49 @@ def getStates():
         reader = csv.reader(csvfile, delimiter = ",")
         for row in reader:
             states.append(row[1])
-    states = states[1:51]
+    states = states[1:2] # '2' for testing purposes -> change '2' to '52' for execution
     return states
 
 
+def pullStateData(state):
+    # Takes state string and returns JSON of bills for state from legiscan
+    params = {'key':'ff6da19238f87945db1c0dd5d6bc1674',
+              'op': 'getMasterList',
+              'state':state}
+    r = requests.get('http://api.legiscan.com/?',params=params)
+    return r
 
-url = 'http://api.legiscan.com/?key=ff6da19238f87945db1c0dd5d6bc1674&op=getMasterList&state=FL'
-request = Request(url)
-try:
-    response = urlopen(request)
-    bills = response.read()
-    print bills[:80]
-except URLError, e:
-    print 'No bills. Got an error code:', e
+
+def editStateBills(request):
+    # Takes state bills request object and edits it to DB specifications, returns two tuples objects)
+    billsDesc = []
+    billsLog = []
+    original = request.json()
+    session = original["masterlist"]["session"]["session_name"]
+    del original["masterlist"]["session"]
+    masterList = original["masterlist"].values() # turns dict into list of bills (gets rid of useless number keys)
+    for bill in masterList:
+        billDesc = {
+            "bill_id":bill["bill_id"],
+            "session": session,
+            "number": bill["number"],
+            "title": bill["title"],
+            "description": bill["description"]
+            }
+        billLog = {
+            "bill_id": bill["bill_id"],
+            "status_date": bill["status_date"],
+            "status": bill["status"],
+            "last_action_date": bill["last_action_date"],
+            "last_action": bill["last_action"]
+        }
+        billsDesc.append(billDesc)
+        billsLog.append(billLog)
+    billsDesc = tuple(billsDesc)
+    billsLog = tuple(billsLog)
+    #Insert method to insert data to database
+
+def getAllStateBills():
+    # Gets full edited bill history of all states from LegiScan
+    for state in getStates():
+        editStateBills(pullStateData(state))
