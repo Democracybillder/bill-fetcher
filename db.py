e084a47268ed4c2354a1916d3afc3fee9b4df58f
DanielGrossman <daniel@swiftkey.net>
2015-04-10 20:04:01 -0400

    integrate fetcher with db, first working version

--- db.py
+++ db.py
@@ -3,36 +3,34 @@
 import psycopg2
 
 
-class FetcherDb(object):
-    """database class"""
-def __init__(self, dbname):
-    self.dbname = dbname
-
-def dbinserttuple(self, query, tup):
+def dbinserttuple(dbname, query, tup):
     """wrapper method to handle db connections and tuple inserts"""
-    conn = psycopg2.connect(dbname=self.dbname)
+    conn = psycopg2.connect(dbname=dbname)
     cur = conn.cursor()
     cur.executemany(query, tup)
-    return conn.commit()
+    conn.commit()
 
-def insertbilldesc(self, tups):
+def insertbilldesc(dbname, tups):
     """inserting bill descriptions to database"""
     query = """ INSERT INTO bills(bill_id, session,
-        official_id, title, "desc") VALUES (%(bill_id)s, %(session)s,
-    %(number)s, %(title)s, %(description)s) """
-    return dbinserttuple(self.dbname, query, tups)
+        official_id, title, state, "desc") VALUES (%(bill_id)s, %(session)s,
+    %(number)s, %(title)s, %(state)s, %(description)s) """
+    dbinserttuple(dbname, query, tups)
 
 
-def insertbilllog(self, tups):
+def insertbilllog(dbname, tups):
     """inserting bill logs to database"""
     query = """ INSERT INTO bill_log(bill_id, status_date,
         status, last_action_date, last_action) VALUES (%(bill_id)s, %(status_date)s,
     %(status)s, %(last_action_date)s, %(last_action)s) """
-    return dbinserttuple(self.dbname, query, tups)
+    dbinserttuple(dbname, query, tups)
 
-def insertbills(self, desc, log):
+
+def insertbills(dbname, desc, log):
     """insert all bill data to database"""
-    return insertbilldesc(self.dbname, desc), insertbilllog(self.dbname, log)
+    insertbilldesc(dbname, desc)
+    insertbilllog(dbname, log)
+
 
 
 
