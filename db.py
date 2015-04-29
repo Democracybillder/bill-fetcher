a69a07a1179c888abe119de609da6f38f54fda25
Doron Shiffer-Sebba <shdoron1@gmail.com>
2015-04-29 12:03:30 -0400

    db layer + new code file to fetch and populate db with session information

--- db.py
+++ db.py
@@ -2,9 +2,6 @@
 '''db layer for state bill fetcher'''
 import psycopg2
 
-
-
-
 def dbinserttuple(dbname, query, tup):
     """wrapper method to handle db connections and tuple inserts"""
     conn = psycopg2.connect(dbname=dbname)
@@ -41,7 +38,6 @@ def insertbilldesc(dbname, tups):
     %(number)s, %(title)s, %(state)s, %(description)s) """
     dbinserttuple(dbname, query, tups)
 
-
 def insertbilllog(dbname, tups):
     """inserting bill logs to database"""
     query = """ INSERT INTO bill_log(bill_id, status_date,
@@ -49,10 +45,14 @@ def insertbilllog(dbname, tups):
     %(status)s, %(last_action_date)s, %(last_action)s) """
     dbinserttuple(dbname, query, tups)
 
-
 def insertbills(dbname, desc, log):
     """insert all bill data to database"""
     insertbilldesc(dbname, desc)
     insertbilllog(dbname, log)
 
-
+def insertsessiondata(dbname, tups):
+    """insert new session data into database"""
+    query = """ INSERT INTO sessions(session_id, year_start,
+    year_end, session) VALUES (%(session_id)s, %(year_start)s,
+    %(year_end)s, %(session)s) """
+    dbinserttuple(dbname, query, tups)
---
+++ fetch_session_years.py
@@ -0,0 +1,46 @@
+
+''' pulls session years data from legiscan api and populates db'''
+
+import csv
+import requests # requests needs to be installed for this to work ($ git clone git://github.com/kennethreitz/requests.git)
+import db
+
+def getStates():
+    '''Returns list of LegiScan Abbreviated State names from LegiScan source csv'''
+    states = []
+    with open('reference-files/state.csv', 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter=",")
+        for row in reader:
+            states.append(row[1])
+    states = states[1:53]
+    return states
+
+def pullStateData(state):
+    '''Takes state string and returns JSON of sessions for state from legiscan'''
+    params = {'key':'ff6da19238f87945db1c0dd5d6bc1674',
+              'op': 'getSessionList',
+              'state':state}
+    req = requests.get('http://api.legiscan.com/?', params=params)
+    return req
+
+def compileStateSessions(request):
+    '''Takes state sessions request objects and edits it to db specifications'''
+    original = request.json()
+    sessions = []
+    for instance in original["sessions"]:
+    	session = {
+    		"session_id":instance["session_id"],
+    		"year_start":instance["year_start"],
+    		"year_end":instance["year_end"],
+    		"session":instance["session_name"]
+    		}
+    	sessions.append(session)
+    sessions = tuple(sessions)
+    database = db
+    database.insertsessiondata('billder',sessions)
+
+def getAllStateSessions():
+	for state in getStates():
+		compileStateSessions(pullStateData(state))
+
+getAllStateSessions()
